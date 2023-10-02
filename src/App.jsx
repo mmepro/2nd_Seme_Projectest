68d3b672eaa6aa53512b5cf8754ab4010486b073
lsh <lee11418@naver.com>
2023-10-02 16:35:37 +0900

    first_commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Box_Recommendation from './Boxoffice_recommendation'
+
+function App() {
+
+  return (
+    <Box_Recommendation/>
+  )
+}
+
+export default App

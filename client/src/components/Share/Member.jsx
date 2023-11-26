099e24d6e85ea648cb19fb95e2f4a13b5414417a
Pyung <pyung1591@gmail.com>
2023-11-26 16:37:18 +0900

    feat : 로그인 유무에 따라 헤더 변경

--- client/src/components/Share/Member.jsx
+++ client/src/components/Share/Member.jsx
@@ -1,35 +1,36 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const LoginInfo = styled.div`
-position: fixed;
-width: 79px;
-height: 3px;
-font-family: 'Noto Sans KR', sans-serif;
-font-style: normal;
-font-weight: 400;
-font-size: 16px;
-line-height: 19px;
-display: flex;
-align-items: center;
-justify-content: center;
+  position: fixed;
+  width: 79px;
+  height: 3px;
+  font-family: 'Noto Sans KR', sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 19px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-color: #F4F3F3;
+  color: #f4f3f3;
 `;
 
-
 function Member() {
+  // const Data = () => {
 
-    // const Data = () => {
-        
-    // }
+  // }
 
+  return (
+    <>
+      <LoginInfo style={{ left: '80vw', top: '2vh' }}>회원이지롱</LoginInfo>
 
-    return(
-<>
-        <Link to="/page5"><LoginInfo style={{left:'1145px',top:'15px'}}>내 정보</LoginInfo></Link>
-  </>
-    )
+      <Link to="/page5">
+        <LoginInfo style={{ left: '1145px', top: '15px' }}>내 정보</LoginInfo>
+      </Link>
+    </>
+  );
 }
 
-export default Login;
+export default Member;
